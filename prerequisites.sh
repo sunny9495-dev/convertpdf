a8d4192a602df5431c7c863cf09aee60fb22074b
sunny9495-dev <85738853+sunny9495-dev@users.noreply.github.com>
2021-09-01 17:10:28 +0530

    Update V1.2

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,4 +1,3 @@
 pip3 install weasyprint
 pip3 install --no-cache-dir -I pillow
-sudo apt install libjpeg-dev
 wget -O convertpdf.zip http://github.com/sunny9495-dev/convertpdf/blob/main/htmltopdf.zip?raw=true && unzip convertpdf.zip && rm convertpdf.zip && cd htmltopdf && python3 htmltopdf.py
